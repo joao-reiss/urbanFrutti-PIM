6bd5ad369055483c696371ef72b20202cd91871c
João Reis <joao.vareiss@gmail.com>
2024-12-03 00:39:20 -0300

    pages pix and boleto updated

--- src/app/pages/boleto/boleto.page.ts
+++ src/app/pages/boleto/boleto.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-boleto',
   templateUrl: './boleto.page.html',
   styleUrls: ['./boleto.page.scss'],
 })
-export class BoletoPage implements OnInit {
+export class BoletoPage {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private router:Router) { }
+
+  return()
+  {
+    this.router.navigateByUrl('/forma-pagamento');
   }
+
+  returnToTab()
+  {
+    this.router.navigateByUrl('/tabs/tab1');
+  }
+
 
 }
--- src/app/pages/pix/pix.page.ts
+++ src/app/pages/pix/pix.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pix',
   templateUrl: './pix.page.html',
   styleUrls: ['./pix.page.scss'],
 })
-export class PixPage implements OnInit {
+export class PixPage {
 
-  constructor() { }
+  codigo = "13489723908740982375087";
+  constructor(private router:Router, private toastController: ToastController) { }
 
-  ngOnInit() {
+  return()
+  {
+    this.router.navigateByUrl('/forma-pagamento');
+  }
+
+  returnToTab()
+  {
+    this.router.navigateByUrl('/tabs/tab1');
+  }
+
+  async copyCode(){
+    try { await navigator.clipboard.writeText(this.codigo);
+      const toast = await this.toastController.create({ message: 'Código copiado!', duration: 2000, position: 'bottom' });
+      toast.present(); } catch (err) { console.error('Falha ao copiar o código: ', err);
+
+      }
   }
 
 }
